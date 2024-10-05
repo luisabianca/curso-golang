360bab814944a5885ff40f98343aa3be1e282629
luisabianca <luisabianca93@gmail.com>
2024-10-04 23:27:27 -0300

    finish package class

--- Packages/main.go
+++ Packages/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"module/assistant"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing from main package")
 	assistant.Towrite()
+
+	erro := checkmail.ValidateFormat("luisabianca93@gmail.com")
+	fmt.Println(erro)
 }
